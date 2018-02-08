7f1cf2b444f31ddc0fbff589da7945f8f091c441
SergiGasso <gasso.vives.s@gmail.com>
2018-02-08 09:14:11 +0100

    bankia

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+//Declaro dos variables per fer el exemple, aquest cas s'anomenen a i b
+
+
+var a=3
+var b=2
+ 
+ //Script amb error
+
+ document.write("<div>Suma: "+a+b+"</div>");
+
+//Script ben declarat
+
+document.write("<div>Suma: ",a+b,"</div>");
